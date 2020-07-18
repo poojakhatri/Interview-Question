d08435442c0b403f13784fd1ec0ebb79f516c2c9
poojakhatri <poojakhatri888@gmail.com>
2020-07-18 08:13:20 +0530

    added new files

---
+++ class/new.py
@@ -0,0 +1,13 @@
+# There is a function called add_book() that takes a non-empty string containing the title of a book to add it to recommendations. 
+# Share your favorite book with us by calling this function. You don't have to declare it, just call it in your code.
+
+# Importantly, specify the keyword argument title in the call. Otherwise, you may get an error.
+
+
+def add_book(title):
+	recommendations=[]
+	recommendations.append(title) 
+	print(recommendations)
+
+title = "Rich Dad Poor Dad'"
+add_book(title)
